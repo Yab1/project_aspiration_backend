d521b96d95b7dee97c4ae479d90fbd0c56d283eb
Yab1 <yabilisanu@gmail.com>
2024-07-10 00:40:35 +0300

    feat: configure PostgreSQL database connection

--- config/django/base.py
+++ config/django/base.py
@@ -90,8 +90,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
+        "HOST": env.str("DB_HOST"),
+        "PORT": env.str("DB_PORT"),
     },
 }
 
