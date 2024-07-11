590d38a6e6b2f467727b42b414aa1ed7413c50f7
Yab1 <yabilisanu@gmail.com>
2024-07-11 23:00:07 +0300

    feat: define URLs for feedbacks app APIs
    Define URL patterns for `feedbacks` app APIs including:
    - FeedbackListApi for listing feedbacks
    - FeedbackCreateApi for creating new feedbacks
    - CheckSimilarity for checking similarity between images

--- core/feedbacks/urls.py
+++ core/feedbacks/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .apis import FeedbackCreate
+from .apis import CheckSimilarity, FeedbackCreateApi, FeedbackListApi
 
 app_name = "feedbacks"
 
 urlpatterns: list[URLPattern] = [
-    path("create/", FeedbackCreate.as_view(), name=""),
+    path("", FeedbackListApi.as_view(), name="feedback-list"),
+    path("create/", FeedbackCreateApi.as_view(), name="feedback-create"),
+    path("check_similarity/", CheckSimilarity.as_view(), name="feedback-check_similarity"),
 ]
