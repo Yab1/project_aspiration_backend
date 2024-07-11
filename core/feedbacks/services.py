797d956006a1c737f8b1a74413d936bec1602606
Yab1 <yabilisanu@gmail.com>
2024-07-12 00:40:36 +0300

    feat: add feedback_create function for signature verification and feedback creation
    Implemented the feedback_create function to verify a user's signature against a reference signature stored in the database. If the similarity between the uploaded and reference signatures exceeds 80%, the function creates a new feedback entry with the user's comment and signature. Otherwise, it raises a PermissionDenied exception. Error handling ensures proper validation and exception management during the process.

--- core/feedbacks/services.py
+++ core/feedbacks/services.py
@@ -1,7 +1,56 @@
+import os
+import tempfile
+
+from django.conf import settings
+from rest_framework.exceptions import PermissionDenied, ValidationError
+
 from core.users.models import Member
 
 from .models import Feedback
+from .utils import compare_images
 
 
 def feedback_create(*, current_user: Member, comment: str, signature):
-    Feedback.objects.create(author=current_user, comment=comment, signature=signature)
+    try:
+        reference_signature_path = Feedback.objects.all()[0].signature
+        reference_signature_full_path = os.path.join(settings.MEDIA_ROOT, reference_signature_path.path)
+
+        # Save the uploaded signature to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as temp_signature:
+            temp_signature.write(signature.read())
+            uploaded_signature_path = temp_signature.name
+
+        similarity = compare_images(image1_path=reference_signature_full_path, image2_path=uploaded_signature_path)
+
+        if similarity <= 80:
+            raise PermissionDenied("Signature verification failed. You are not authorized to create feedback.")
+
+        Feedback.objects.create(author=current_user, comment=comment, signature=signature)
+
+        return "Signature verified successfully. Feedback has been created."
+
+    except PermissionDenied as pd:
+        raise pd
+
+    except ValueError as ve:
+        raise ValidationError(f"Validation error: {ve}")
+
+    except Exception as e:
+        raise ValidationError(f"Unexpected error: {e}")
+
+
+def check_similarity(*, image_1, image_2):
+    # Save the uploaded images to a temporary files
+    with (
+        tempfile.NamedTemporaryFile(delete=False) as temp_file_1,
+        tempfile.NamedTemporaryFile(delete=False) as temp_file_2,
+    ):
+        # Write the content of image_1 to temp_file_1
+        temp_file_1.write(image_1.read())
+        image1_path = temp_file_1.name
+
+        # Write the content of image_2 to temp_file_2
+        temp_file_2.write(image_2.read())
+        image2_path = temp_file_2.name
+
+    return compare_images(image1_path, image2_path)
