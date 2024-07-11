ae70e8f64caab4402318a55f7856d49579f64987
Yab1 <yabilisanu@gmail.com>
2024-07-11 23:06:27 +0300

    feat: implement FeedbackListApi, FeedbackCreateApi, and CheckSimilarity API views
    Implement API views for:
    - FeedbackListApi: Retrieves a list of feedbacks with author details, comment, and signature.
    - FeedbackCreateApi: Creates a new feedback entry with comment and signature fields.
    - CheckSimilarity: Computes similarity between two base64-encoded images.
    
    These views utilize Django REST Framework serializers and handle validations and error handling accordingly.

---
+++ core/feedbacks/apis.py
@@ -0,0 +1,76 @@
+from drf_extra_fields.fields import Base64ImageField
+from rest_framework import serializers
+from rest_framework import status as http_status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.common.utils import inline_serializer
+from core.users.models import Member
+
+from .models import Feedback
+from .services import check_similarity, feedback_create
+
+
+class CheckSimilarity(APIView):
+    class InputSerializer(serializers.Serializer):
+        image_1 = Base64ImageField()
+        image_2 = Base64ImageField()
+
+    def post(self, request) -> Response:
+        input_serializer = self.InputSerializer(data=request.data)
+
+        input_serializer.is_valid(raise_exception=True)
+
+        try:
+            similarity = check_similarity(**input_serializer.validated_data)
+
+            response_data = {"data": {"similarity": f"{similarity:.2f}"}}
+            return Response(data=response_data, status=http_status.HTTP_200_OK)
+        except ValueError as e:
+            raise ValidationError(e)
+
+        except Exception as e:
+            raise ValidationError(e)
+
+
+class FeedbackListApi(APIView):
+    class OutputSerializer(serializers.Serializer):
+        author = inline_serializer(
+            fields={"full_name": serializers.CharField()},
+        )
+        comment = serializers.CharField()
+        signature = serializers.URLField()
+
+    def get(self, request) -> Response:
+        feedbacks = Feedback.objects.all()
+
+        output_serializer = self.OutputSerializer(feedbacks, many=True)
+
+        response_data = {"data": output_serializer.data}
+
+        return Response(data=response_data, status=http_status.HTTP_200_OK)
+
+
+class FeedbackCreateApi(APIView):
+    class InputSerializer(serializers.Serializer):
+        comment = serializers.CharField()
+        signature = Base64ImageField()
+
+    serializer_class = InputSerializer
+
+    def post(self, request) -> Response:
+        current_user = Member.objects.all()[0]
+        input_serializer = self.InputSerializer(data=request.data)
+
+        input_serializer.is_valid(raise_exception=True)
+
+        try:
+            feedback_create(current_user=current_user, **input_serializer.validated_data)
+
+            return Response(status=http_status.HTTP_200_OK)
+        except ValueError as e:
+            raise ValidationError(e)
+
+        except Exception as e:
+            raise ValidationError(e)
